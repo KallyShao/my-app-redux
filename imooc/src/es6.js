4805f06da7c8b4b1c8c512acb53bd12a5a838c2d
Kally Shao <188963758@qq.com>
2018-05-11 17:48:51 +0800

    initial commit

---
+++ imooc/src/es6.js
@@ -0,0 +1,12 @@
+/*
+* @Author: Administrator
+* @Date:   2018-05-11 17:24:35
+* @Last Modified by:   Administrator
+* @Last Modified time: 2018-05-11 17:39:00
+*/
+
+let obj = {
+	name: 'kally',
+	age: 18
+}
+console.log(Object.entries(obj));
