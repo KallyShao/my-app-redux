e80f5d107f11971b707a00b6cff0a9d41e57978b
Kally Shao <188963758@qq.com>
2018-05-16 17:16:33 +0800

    m

--- imooc/src/es6.js
+++ imooc/src/es6.js
@@ -2,11 +2,14 @@
 * @Author: Administrator
 * @Date:   2018-05-11 17:24:35
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-11 17:39:00
+* @Last Modified time: 2018-05-16 10:40:11
 */
 
 let obj = {
 	name: 'kally',
 	age: 18
 }
-console.log(Object.entries(obj));
+let name = 'imooc';
+let obj1 = {
+	[name]: 'hello'
+}
--- imooc/src/index.js
+++ imooc/src/index.js
@@ -1,9 +1,88 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import './es6.js';
-import registerServiceWorker from './registerServiceWorker';
+/*
+* @Author: Administrator
+* @Date:   2018-05-16 13:34:46
+* @Last Modified by:   Administrator
+* @Last 
+Modified time: 2018-05-16 13:34:46
+*/
+const appState = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red',
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+function renderApp(appState){
+	console.log('render app...');
+	renderTitle(appState.title);
+	renderContent(appState.content);
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+function renderTitle(title){
+	console.log('render title...');
+	const titleDOM = document.getElementById('title');
+	titleDOM.innerHTML = title.text;
+	titleDOM.style.color = title.color;
+}
+
+function renderContent(content){
+	console.log('render content...');
+	const contentDOM = document.getElementById('content');
+	contentDOM.innerHTML = content.text;
+	contentDOM.style.color = content.color;
+}
+
+// function dispatch(action){
+// 	switch(action.type){
+// 		case 'UPDATE_TITLE_TEXT':
+// 			appState.title.text = action.text;
+// 			break;
+// 		case 'UPDATE_TITLE_COLOR':
+// 			appState.title.color = action.color;
+// 			break;
+// 		default: 
+// 			break;
+// 	}
+// }
+
+//createStore
+function createStore(state, stateChanger){
+	const listeners = [];
+	const subscribe = (listener) => listeners.push(listener);
+	const getState = () => state; //state是传进来的回调函数
+	const dispatch = (action) => {
+		stateChanger(state, action);
+		listeners.forEach((listener) => listener());
+	};
+	return {getState, dispatch, subscribe};
+}
+
+function stateChanger(state, action){
+	switch(action.type){
+		case 'UPDATE_TITLE_TEXT':
+			appState.title.text = action.text;
+			break;
+		case 'UPDATE_TITLE_COLOR':
+			appState.title.color = action.color;
+			break;
+		default: 
+			break;
+	}
+}
+const store = createStore(appState, stateChanger);
+store.subscribe(() => renderApp(store.getState()));
+
+renderApp(store.getState());   //初始化渲染
+store.dispatch({ 
+	type: 'UPDATE_TITLE_TEXT',
+	text: '《React.js 小书》'
+})
+store.dispatch({
+	type: 'UPDATE_TITLE_COLOR',
+	color: 'green'
+})
+
+
