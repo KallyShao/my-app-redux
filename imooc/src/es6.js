74a216e60c48365fed4aa924bc02a82a517ad75d
Kally Shao <188963758@qq.com>
2018-05-17 17:36:28 +0800

    m

--- imooc/src/es6.js
+++ imooc/src/es6.js
@@ -2,14 +2,15 @@
 * @Author: Administrator
 * @Date:   2018-05-11 17:24:35
 * @Last Modified by:   Administrator
-* @Last Modified time: 2018-05-16 10:40:11
+* @Last Modified time: 2018-05-17 10:25:16
 */
 
 let obj = {
 	name: 'kally',
 	age: 18
 }
-let name = 'imooc';
 let obj1 = {
-	[name]: 'hello'
-}
+	name1: 'aa',
+	...obj
+};
+console.log(obj1);
--- imooc/src/index.js
+++ imooc/src/index.js
@@ -5,34 +5,39 @@
 * @Last 
 Modified time: 2018-05-16 13:34:46
 */
-const appState = {
-  title: {
-    text: 'React.js 小书',
-    color: 'red',
-  },
-  content: {
-    text: 'React.js 小书内容',
-    color: 'blue'
-  }
-}
-function renderApp(appState){
+// import './es6.js';
+
+// const appState = {
+//   title: {
+//     text: 'React.js 小书',
+//     color: 'red',
+//   },
+//   content: {
+//     text: 'React.js 小书内容',
+//     color: 'blue'
+//   }
+// }
+function renderApp(newAppState, oldAppState = {}){
+	if(newAppState === oldAppState) return;
 	console.log('render app...');
-	renderTitle(appState.title);
-	renderContent(appState.content);
+	renderTitle(newAppState.title, oldAppState);
+	renderContent(newAppState.content, oldAppState.content);
 }
 
-function renderTitle(title){
+function renderTitle(newTitle, oldTitle = {}){
+	if(newTitle === oldTitle) return;
 	console.log('render title...');
 	const titleDOM = document.getElementById('title');
-	titleDOM.innerHTML = title.text;
-	titleDOM.style.color = title.color;
+	titleDOM.innerHTML = newTitle.text;
+	titleDOM.style.color = newTitle.color;
 }
 
-function renderContent(content){
+function renderContent(newContent, oldContent = {}){
+	if(newContent === oldContent) return;
 	console.log('render content...');
 	const contentDOM = document.getElementById('content');
-	contentDOM.innerHTML = content.text;
-	contentDOM.style.color = content.color;
+	contentDOM.innerHTML = newContent.text;
+	contentDOM.style.color = newContent.color;
 }
 
 // function dispatch(action){
@@ -49,31 +54,59 @@ function renderContent(content){
 // }
 
 //createStore
-function createStore(state, stateChanger){
+function createStore(reducer){
+	let state = null;
 	const listeners = [];
 	const subscribe = (listener) => listeners.push(listener);
 	const getState = () => state; //state是传进来的回调函数
 	const dispatch = (action) => {
-		stateChanger(state, action);
+		state = reducer(state, action);
 		listeners.forEach((listener) => listener());
 	};
+	dispatch({}); //初始化state
 	return {getState, dispatch, subscribe};
 }
 
-function stateChanger(state, action){
+function reducer(state, action){
+	if(!state){
+		return {
+			title: {
+			  text: 'React.js 小书',
+			  color: 'red',
+			},
+			content: {
+			  text: 'React.js 小书内容',
+			  color: 'blue'
+			}
+		}
+	}
 	switch(action.type){
 		case 'UPDATE_TITLE_TEXT':
-			appState.title.text = action.text;
-			break;
+			return {
+				...state,
+				text: action.text
+			};
 		case 'UPDATE_TITLE_COLOR':
-			appState.title.color = action.color;
-			break;
+			return {
+				...state,
+				color: action.color
+			};
+		case 'UPDATE_CONTENT_COLOR':
+			return {
+				...state,
+				color: action.color
+			};
 		default: 
-			break;
+			return state;
 	}
 }
-const store = createStore(appState, stateChanger);
-store.subscribe(() => renderApp(store.getState()));
+const store = createStore(appState, reducer);
+let oldState = store.getState();
+store.subscribe(() => {
+	const newState = store.getState();
+	renderApp(newState, oldState);
+	oldState = newState;
+});
 
 renderApp(store.getState());   //初始化渲染
 store.dispatch({ 
@@ -84,5 +117,8 @@ store.dispatch({
 	type: 'UPDATE_TITLE_COLOR',
 	color: 'green'
 })
-
+store.dispatch({
+	type: 'UPDATE_CONTENT_COLOR',
+	color: 'orange'
+})
 
